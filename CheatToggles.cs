6f08612c8f4aafe71d419abe5d6d05829b7cb750
Chris <itzchwis@gmail.com>
2026-03-01 19:47:23 -0600

    Update CheatToggles.cs

--- CheatToggles.cs
+++ CheatToggles.cs
@@ -14,33 +14,28 @@ namespace NekoMenu
         public static bool showRoles = true;
         public static bool showDistance = true;
         
-        // Player toggles
+        // Kill toggles
+        public static bool zeroKillCd = false;
+        public static int selectedTargetId = -1;
+        public static bool killSelected = false;
+        public static bool killSelf = false;
         public static bool killAll = false;
         public static bool killAllCrew = false;
         public static bool killAllImps = false;
-        public static bool killAllLobby = false; // Experimental
-        public static bool teleportCursor = false;
-        public static bool noClip = false;
-        public static bool fakeRevive = false;
+        public static bool killAllLobby = false;
         
         // Role toggles
         public static bool endlessVentTime = false;
         public static bool noVentCooldown = false;
+        public static bool useVents = false;
+        public static bool walkVent = false;
+        public static bool kickVents = false;
         public static bool endlessSsDuration = false;
         public static bool noVitalsCooldown = false;
         public static bool endlessBattery = false;
         public static bool noTrackingCooldown = false;
         public static bool noTrackingDelay = false;
         public static bool endlessTracking = false;
-        public static bool useVents = false;
-        public static bool walkVent = false;
-        public static bool kickVents = false;
-        
-        // Kill toggles
-        public static bool zeroKillCd = false;
-        public static int selectedTargetId = -1;
-        public static bool killSelected = false;
-        public static bool killSelf = false;
         
         // Meeting toggles
         public static bool closeMeeting = false;
@@ -48,9 +43,14 @@ namespace NekoMenu
         public static bool callMeeting = false;
         public static bool forceStartGame = false;
         public static bool sabotageMap = false;
-        
-        // Tasks
         public static bool completeMyTasks = false;
+        
+        // Movement toggles
+        public static bool noClip = false;
+        public static bool teleportCursor = false;
+        public static bool speedHackEnabled = false;
+        public static float speedMultiplier = 2f;
+        public static bool fakeRevive = false;
         
         // Sabotage toggles
         public static bool sabotageLights = false;
@@ -58,12 +58,21 @@ namespace NekoMenu
         public static bool sabotageO2 = false;
         public static bool sabotageReactor = false;
         public static bool sabotageHeli = false;
-
-        // Speed hack
-        public static float speedMultiplier = 2f;
-        public static bool speedHackEnabled = false;
         
-        // List for protect cheat
+        // Chaos toggles
+        public static string customNotificationText = "";
+        public static bool sendCustomNotification = false;
+        public static bool fakeReport = false;
+        public static int fakeReportTargetId = -1;
+        public static bool teleportAllToMe = false;
+        public static bool swapNames = false;
+        public static bool fakeMeetingFlash = false;
+        public static bool fakeDeathScreen = false;
+        public static int fakeDeathTargetId = -1;
+        public static bool fakeWinScreen = false;
+        public static int fakeWinTeam = 0;
+        
+        // Protect list
         public static List<PlayerControl> playersToProtect = new List<PlayerControl>();
     }
 }
